87495bdc92e84adad35799f4ef14330b7bea0bb9
m8524769 <m8524769@163.com>
2020-03-29 16:48:38 +0800

    Init

---
+++ contracts/untitled/include/untitled.hpp
@@ -0,0 +1,21 @@
+#include <eosio/eosio.hpp>
+
+using namespace std;
+using namespace eosio;
+
+CONTRACT untitled : public contract {
+  public:
+    using contract::contract;
+
+    ACTION hi(name from, string message);
+    ACTION bye(name from, string message);
+    ACTION clear();
+
+  private:
+    TABLE messages {
+      name    user;
+      string  text;
+      auto primary_key() const { return user.value; }
+    };
+    typedef multi_index<name("messages"), messages> messages_table;
+};
