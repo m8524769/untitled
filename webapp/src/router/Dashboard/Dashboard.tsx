410a558fad5c35bce3b361cdb63e1e24b7ce6d0b
m8524769 <m8524769@163.com>
2020-04-01 02:25:03 +0800

    Update style

--- webapp/src/router/Dashboard/Dashboard.tsx
+++ webapp/src/router/Dashboard/Dashboard.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Button, PageHeader, Space } from 'antd';
+import { Button, PageHeader, Space, Tabs, Skeleton, Input } from 'antd';
 import IpfsUpload from './component/IpfsUpload';
 import PublishFile from './component/PublishFile';
 import Title from 'antd/lib/typography/Title';
 import { SyncOutlined } from '@ant-design/icons';
+import Paragraph from 'antd/lib/typography/Paragraph';
 
 const Dashboard: React.FC = () => {
   return (
@@ -11,12 +12,13 @@ const Dashboard: React.FC = () => {
       style={{
         display: 'grid',
         gridTemplateColumns: 'repeat(3, 1fr)',
-        // gridGap: '10px',
+        gridAutoRows: 'minmax(auto, 230px)',
       }}
     >
       <PageHeader
         title="My Wallet"
-        extra={<Button shape="circle" icon={<SyncOutlined />} />}
+        subTitle="Update 3m 45s ago"
+        extra={<Button shape="round" icon={<SyncOutlined />} />}
         style={{
           gridColumn: '1 / 2',
           gridRow: '1',
@@ -24,6 +26,7 @@ const Dashboard: React.FC = () => {
         }}
       >
         <Title level={2}>45.2310 BTC</Title>
+        <Paragraph>≈＄8371928.17</Paragraph>
         <Space>
           <Button>Withdraw</Button>
           <Button>Recharge</Button>
@@ -33,32 +36,54 @@ const Dashboard: React.FC = () => {
 
       <PageHeader
         title="Recent Transactions"
-        extra={<Button shape="circle" icon={<SyncOutlined />} />}
+        extra={<Button shape="round" icon={<SyncOutlined />} />}
         style={{
           gridColumn: '2 / 3',
           gridRow: '1',
         }}
-      ></PageHeader>
-
-      <PageHeader
-        title="IPFS Upload"
-        style={{
-          gridColumn: '3 / 4',
-          gridRow: '1',
-        }}
       >
-        <IpfsUpload></IpfsUpload>
+        <Skeleton active />
       </PageHeader>
 
-      <PageHeader
-        title="Publish File"
+      <div
         style={{
           gridColumn: '3 / 4',
-          gridRow: '2 / 4',
+          gridRow: '1 / 5',
         }}
       >
-        <PublishFile></PublishFile>
-      </PageHeader>
+        <PageHeader title="IPFS Upload">
+          <IpfsUpload></IpfsUpload>
+        </PageHeader>
+
+        <PageHeader title="Publish File">
+          <PublishFile></PublishFile>
+        </PageHeader>
+      </div>
+
+      <Tabs
+        defaultActiveKey="1"
+        tabBarExtraContent={
+          <Input.Search
+            placeholder="Search files"
+            onSearch={(value: string) => {}}
+            enterButton
+          />
+        }
+        style={{
+          gridColumn: '1 / 3',
+          gridRow: '2 / 5',
+          padding: '16px 24px',
+        }}
+      >
+        <Tabs.TabPane tab="Bought Files" key="1">
+          <Skeleton active />
+          <Skeleton active />
+        </Tabs.TabPane>
+        <Tabs.TabPane tab="Published Files" key="2">
+          <Skeleton active />
+          <Skeleton active />
+        </Tabs.TabPane>
+      </Tabs>
     </div>
   );
 };
