3b5ad225d72411b4416e4b4391e8aac358a60253
m8524769 <m8524769@163.com>
2020-04-07 20:28:57 +0800

    Feat: File creating & transaction

--- contracts/untitled/include/untitled.hpp
+++ contracts/untitled/include/untitled.hpp
@@ -9,55 +9,28 @@ class [[eosio::contract("untitled")]] untitled : public contract {
     using contract::contract;
 
     [[eosio::action]]
-    void hi(name from, string message);
+    void createfile(name owner, string cid, string description, asset price);
 
     [[eosio::action]]
-    void clear();
-
-    [[eosio::action]]
-    void clearuser();
-
-    [[eosio::action]]
-    void inituser(name user, string info);
-
-    [[eosio::action]]
-    void createfile(string cid, string description, asset price);
-
-    [[eosio::action]]
-    void buyfile(name buyer, uint64_t id);
+    void sellfile(uint64_t id, asset price);
 
     [[eosio::action]]
     void clearfile();
 
-    // 监听 eosio.token 的 transfer 操作
-    // [[eosio::on_notify("eosio.token::transfer")]]
-    // void on_transfer(name from, name to, asset quantity, string memo);
+    [[eosio::on_notify("eosio.token::transfer")]]
+    void on_transfer(name from, name to, asset quantity, string memo);
 
   private:
-    struct [[eosio::table]] message {
-      name    user;
-      string  text;
-      auto primary_key() const { return user.value; }
-    };
-
-    struct [[eosio::table]] user {
-      name    user;
-      string  info;
-      asset   balance;
-      auto primary_key() const { return user.value; }
-    };
-
     struct [[eosio::table]] file {
+      uint64_t  id;
       name      owner;
       string    cid;
       string    description;
       uint64_t  size;
+      bool      for_sale;
       asset     price;
-      auto primary_key() const { return cid.code().raw(); }
+      auto primary_key() const { return id; }
     };
-
-    typedef multi_index<name("messages"), message> messages_table;
-    typedef multi_index<name("users"), user> users_table;
 
     typedef multi_index<name("files"), file> files_table;
 };
--- contracts/untitled/src/untitled.cpp
+++ contracts/untitled/src/untitled.cpp
@@ -1,134 +1,71 @@
 #include <untitled.hpp>
 
-ACTION untitled::hi(name from, string message) {
-  require_auth(from);
+[[eosio::action]]
+void untitled::createfile(name owner, string cid, string description, asset price) {
+  require_auth( owner );
 
-  // Init the _message table
-  messages_table _messages(get_self(), get_self().value);
+  files_table files(get_self(), get_self().value);
 
-  // Find the record from _messages table
-  auto msg_itr = _messages.find(from.value);
-  if (msg_itr == _messages.end()) {
-    // Create a message record if it does not exist
-    _messages.emplace(from, [&](auto& msg) {
-      msg.user = from;
-      msg.text = message;
-    });
-  } else {
-    // Modify a message record if it exists
-    _messages.modify(msg_itr, from, [&](auto& msg) {
-      msg.text = message;
-    });
+  bool existing = false;
+  for (auto file_itr = files.begin(); file_itr != files.end() && existing != true; ++file_itr) {
+    if (file_itr->cid == cid) {
+      existing = true;
+    }
   }
-}
+  check(existing == false, "This file is already existing");
 
-ACTION untitled::clear() {
-  require_auth(get_self());
-
-  messages_table _messages(get_self(), get_self().value);
-
-  // Delete all records in _messages table
-  auto msg_itr = _messages.begin();
-  while (msg_itr != _messages.end()) {
-    msg_itr = _messages.erase(msg_itr);
-  }
-}
-
-ACTION untitled::inituser(name user, string info) {
-  require_auth( user );
-
-  users_table users(get_self(), get_self().value);
-
-  auto user_itr = users.find(user.value);
-  if (user_itr == users.end()) {
-    users.emplace(user, [&](auto& row) {
-      row.user = user;
-      row.info = info;
-      row.balance = asset(0, symbol("ASS", 4));
-    });
-  } else {
-    users.modify(user_itr, user, [&](auto& row) {
-      row.info = info;
-    });
-  }
-}
-
-ACTION untitled::clearuser() {
-  require_auth(get_self());
-  users_table users(get_self(), get_self().value);
-  auto user_itr = users.begin();
-  while (user_itr != users.end()) {
-    user_itr = users.erase(user_itr);
-  }
+  files.emplace(owner, [&](auto &file) {
+    file.id = files.available_primary_key();
+    file.owner = owner;
+    file.cid = cid;
+    file.description = description;
+    file.for_sale = true;
+    file.price = price;
+  });
 }
 
 [[eosio::action]]
-void untitled::createfile(string cid, string description, asset price) {
-  require_auth( get_self() );
-
+void untitled::sellfile(uint64_t id, asset price) {
   files_table files(get_self(), get_self().value);
-  files.emplace(get_self(), [&](auto& row) {
-    row.owner = get_self();
-    row.cid = cid;
-    row.description = description;
-    row.price = price;
+
+  auto file_itr = files.find(id);
+  check(file_itr != files.end(), "File does not exist");
+
+  require_auth( file_itr->owner );
+
+  files.modify(file_itr, file_itr->owner, [&](auto &file) {
+    file.for_sale = true;
+    file.price = price;
   });
 }
 
 [[eosio::action]]
 void untitled::clearfile() {
   require_auth( get_self() );
+
   files_table files(get_self(), get_self().value);
+
   auto file_itr = files.begin();
   while (file_itr != files.end()) {
     file_itr = files.erase(file_itr);
   }
 }
 
-[[eosio::action]]
-void untitled::buyfile(name buyer, uint64_t id) {
-  // require_auth( buyer );
-
+[[eosio::on_notify("eosio.token::transfer")]]
+void untitled::on_transfer(name from, name to, asset quantity, string memo) {
   files_table files(get_self(), get_self().value);
-  auto file_itr = files.find(id);
-  check( file_itr != files.end(), "no such file" );
 
-  const auto& file = *file_itr;
-  // check( get_self().balance >= *file.price, "insufficient balance" );
+  // Find file by memo (convert string to uint64_t)
+  auto file_itr = files.find(stoull(memo));
+  check(file_itr != files.end(), "File does not exist");
 
-  // token::transfer_action transfer("token"_n, {get_self(), "active"_n});
-  // transfer.send(get_self(), file.owner, file.price, "");
+  check(file_itr->for_sale == true, "This file is not for sale");
+  check(to == file_itr->owner, "Illegal file transaction");
+  check(quantity.symbol == file_itr->price.symbol, "Illegal asset symbol");
+  check(quantity.amount >= file_itr->price.amount, "Insufficient amount");
 
-  action{
-    permission_level{get_self(), "active"_n},
-    "eosio.token"_n,
-    "transfer"_n,
-    std::make_tuple(get_self(), file.owner, file.price, "")
-  }.send();
+  files.modify(file_itr, from, [&](auto &file) {
+    file.owner = from;
+    file.for_sale = false;
+  });
 }
-
-// EOSIO_DISPATCH(untitled, (hi)(clear)(clearuser)(inituser)(buyfile))
-
-// void untitled::on_transfer(name from, name to, asset quantity, string memo) {
-//   // Validate transaction participants and escape
-//   if (from != get_self() || to == get_self()) return;
-//   // Define token symbol
-//   symbol token_symbol("ASS", 4);
-//   // Validate contract state and arguments
-//   check(quantity.amount > 0, "Insufficient value");
-//   check(quantity.symbol == token_symbol, "Illegal asset symbol");
-//   // Find the members wallet
-//   users_table users(get_self(), get_self().value);
-
-//   auto from_itr = users.find(from.value);
-//   if (from_itr != users.end()) {
-//     users.modify(from_itr, get_self(), [&](auto &row) {
-//       row.balance += quantity;
-//     });
-//   } else {
-//     users.emplace(get_self(), [&](auto &row) {
-//       row.user = to;
-//       row.balance = quantity;
-//     });
-//   }
-// }
