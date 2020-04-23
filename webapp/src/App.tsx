6fa2dabd6655d6f5d9cd905da8b5609307717f39
m8524769 <m8524769@163.com>
2020-04-23 18:06:05 +0800

    Add UAL dependencies

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -5,8 +5,37 @@ import BasicLayout from './layout/BasicLayout';
 import router from './router/router';
 import { AuthProvider } from 'context/AuthContext';
 import { IpfsProvider } from 'context/IpfsContext';
+// import { NETWORK } from 'constants/eos';
+
+// UAL
+// import { UALProvider } from 'ual-reactjs-renderer';
+// import { Ledger } from 'ual-ledger';
+// import { Scatter } from 'ual-scatter';
+// import { Anchor } from 'ual-anchor';
+// import { Lynx } from 'ual-lynx';
+// import { TokenPocket } from 'ual-token-pocket';
 
 import './App.css';
+
+// const appName = 'Untitled';
+// const defaultChain = {
+//   chainId: NETWORK.chainId,
+//   rpcEndpoints: [
+//     {
+//       protocol: NETWORK.protocol,
+//       host: NETWORK.host,
+//       port: NETWORK.port,
+//     },
+//   ],
+// };
+// const chains = [defaultChain];
+
+// Supported Wallets
+// const ledger = new Ledger(chains, { appName });
+// const scatter = new Scatter(chains, { appName });
+// const anchor = new Anchor(chains, { appName });
+// const lynx = new Lynx(chains);
+// const tokenPocket = new TokenPocket(chains);
 
 const App: React.FC = () => {
   return (
@@ -17,6 +46,11 @@ const App: React.FC = () => {
         </IpfsProvider>
       </AuthProvider>
     </ConfigProvider>
+    // <UALProvider
+    //   appName={appName}
+    //   chains={chains}
+    //   authenticators={[ledger, scatter, anchor, lynx, tokenPocket]}
+    // />
   );
 };
 
